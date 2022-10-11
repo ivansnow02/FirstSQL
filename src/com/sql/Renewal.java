7e3e917819543a1157077d91328d0d824ca83d9a
ivansnow02 <ivansnow02@gmail.com>
2022-10-11 16:05:49 +0800

    更新 10.11

--- src/com/sql/Renewal.java
+++ src/com/sql/Renewal.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 public class Renewal {
     static Connection con;
     static PreparedStatement sql;
-    static ResultSet res;
 
     public Connection getConnection() {
         try {
@@ -28,10 +27,9 @@ public class Renewal {
         Renewal r = new Renewal();
         con = r.getConnection();
         Scanner input = new Scanner(System.in);
-        System.out.println("请选择你要查询的表:");
+        System.out.println("请选择你要操作的表:");
         System.out.println("1、用户表");
         System.out.println("2、食物表");
-        System.out.println("3、收藏表");
 
         int flag = input.nextInt();
 
@@ -76,13 +74,49 @@ public class Renewal {
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
-
                 }
                 case 2 -> {
+                    try {
+                        switch (i) {
+                            case 1 -> {
+                                System.out.println("请输入添加内容(name,canteen,floor)：");
+                                String s1 = input.next();
+                                String s2 = input.next();
+                                String s3 = input.next();
+                                sql = con.prepareStatement("insert into tb_food (name,canteen,floor) value (?,?,?)");
+                                sql.setString(1, s1);
+                                sql.setString(2, s2);
+                                sql.setString(3, s3);
+                                sql.executeUpdate();//增加
 
+                            }
+                            case 2 -> {
+                                System.out.println("请输入修改id：");
+                                int id = input.nextInt();
+                                System.out.println("请输入要修改的字段(name,canteen,floor)：");
+                                String word = input.next();
+                                System.out.println("请输入要修改的内容：");
+                                String s = input.next();
+                                sql = con.prepareStatement("update tb_food set "+word+" = '"+s+"' where id = ?");
+                                sql.setInt(1, id);
+                                sql.executeUpdate();
+
+                                //修改
+                            }
+                            case 3 -> {//删除
+                                System.out.println("请输入删除的id：");
+                                int id = input.nextInt();
+                                sql.executeUpdate("delete from tb_food where id = ?");
+                                sql.setInt(1,id);
+                            }
+                        }
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
-                case 3 -> {
-                }
+
+
             }
 
         } catch (Exception e) {
